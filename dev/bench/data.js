6b65f1bb41d70263115a6e1135f16fb0ac38ce45
Stockless <mar.stockle@gmail.com>
2024-12-30 16:46:23 -0300

    clear js history

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -13,58 +13,6 @@ window.BENCHMARK_DATA = {
             "name": "Stockless",
             "username": "Stockless"
           },
-          "id": "eb546fc3c8ac7be0a3f2a4b935def0a1373f92e5",
-          "message": "Test1",
-          "timestamp": "2024-12-30T19:25:40Z",
-          "url": "https://github.com/Stockless/pr-execution-leaderboard/pull/4/commits/eb546fc3c8ac7be0a3f2a4b935def0a1373f92e5"
-        },
-        "date": 1735586873181,
-        "tool": "julia",
-        "benches": [
-          {
-            "name": "Stockless/test_name",
-            "value": 2.785,
-            "unit": "ns",
-            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Stockless",
-            "username": "Stockless"
-          },
-          "committer": {
-            "name": "Stockless",
-            "username": "Stockless"
-          },
-          "id": "26ddd99d94ddf165b227d28fc23f1ea200ea2dcf",
-          "message": "Test1",
-          "timestamp": "2024-12-30T19:25:40Z",
-          "url": "https://github.com/Stockless/pr-execution-leaderboard/pull/4/commits/26ddd99d94ddf165b227d28fc23f1ea200ea2dcf"
-        },
-        "date": 1735587092659,
-        "tool": "julia",
-        "benches": [
-          {
-            "name": "Stockless/test_name",
-            "value": 1.553,
-            "unit": "ns",
-            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "name": "Stockless",
-            "username": "Stockless"
-          },
-          "committer": {
-            "name": "Stockless",
-            "username": "Stockless"
-          },
           "id": "841cde4c5eddbac61f4cf8ae43b69b25727ebba7",
           "message": "Test1",
           "timestamp": "2024-12-30T19:41:29Z",
