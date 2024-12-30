335982ce8f6b855fc934c6721e2edbd12d7c5815
Carlos Castillo Passi <cncastillo@uc.cl>
2024-12-30 17:16:05 -0300

    Try without fork

--- src/MyModule.jl
+++ src/MyModule.jl
@@ -3,7 +3,7 @@ module MyModule
 export expensive_computation, fast_function
 # A simple function to demonstrate benchmarking
 function expensive_computation()
-    n = 30
+    n = 40
     if n == 1
         return 0
     elseif n == 2
