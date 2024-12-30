6ca4251922c5aa0ce193895c60aa4bd2a206736d
github-action-benchmark <github@users.noreply.github.com>
2024-12-30 18:27:16 +0000

    add Julia benchmark result (julia) benchmark result for 296e1872bd31796e0c4b58cedca936fdbb583bd2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735582705365,
+  "lastUpdate": 1735583236190,
   "repoUrl": "https://github.com/Stockless/pr-execution-leaderboard",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "ec/30",
             "value": 2.795,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "committer": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "id": "296e1872bd31796e0c4b58cedca936fdbb583bd2",
+          "message": "Test1",
+          "timestamp": "2024-12-30T17:57:51Z",
+          "url": "https://github.com/Stockless/pr-execution-leaderboard/pull/4/commits/296e1872bd31796e0c4b58cedca936fdbb583bd2"
+        },
+        "date": 1735583235796,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "ec/10",
+            "value": 2.786,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "ec/20",
+            "value": 2.786,
             "unit": "ns",
             "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
