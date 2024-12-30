e203248bd9e1756f693f68c1d21806992cf2f4da
github-action-benchmark <github@users.noreply.github.com>
2024-12-30 19:08:53 +0000

    add Julia benchmark result (julia) benchmark result for 37b97e235c8ec0b45daa49a1fbc2f5c9e602bd67

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735584039143,
+  "lastUpdate": 1735585733448,
   "repoUrl": "https://github.com/Stockless/pr-execution-leaderboard",
   "entries": {
     "Julia benchmark result": [
@@ -87,6 +87,32 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ec/10",
+            "value": 2.795,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "committer": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "id": "37b97e235c8ec0b45daa49a1fbc2f5c9e602bd67",
+          "message": "Test1",
+          "timestamp": "2024-12-30T19:06:50Z",
+          "url": "https://github.com/Stockless/pr-execution-leaderboard/pull/4/commits/37b97e235c8ec0b45daa49a1fbc2f5c9e602bd67"
+        },
+        "date": 1735585733152,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Stockless/20",
             "value": 2.795,
             "unit": "ns",
             "extra": "gctime=0\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
