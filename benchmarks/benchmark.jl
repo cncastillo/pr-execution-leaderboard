9d85c5b586f06e88b306665327ec3cd37edc1d97
Stockless <mar.stockle@gmail.com>
2024-12-30 14:39:45 -0300

    test1

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -3,7 +3,7 @@ push!(LOAD_PATH, "src")
 using MyModule  # Import your module
 
 # Benchmark the expensive_computation function
-results = @benchmark expensive_computation(20)  # Benchmark for n = 30
+results = @benchmark expensive_computation(30)  # Benchmark for n = 30
 
 # Save the benchmark results to a JSON file
 BenchmarkTools.save("benchmarks/benchmark_results.json", results)
