71c8a670046f0e12ff602ae1a03bdd9288a55931
Stockless <mar.stockle@gmail.com>
2024-12-30 14:59:22 -0300

    test800

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -4,8 +4,8 @@ using MyModule  # Import your module
 
 suite = BenchmarkGroup()
 suite["ec"] = BenchmarkGroup(["tag1","tag2"])
-suite["ec"][20] = @benchmarkable expensive_computation(20)
-suite["ec"][30] = @benchmarkable expensive_computation(30)
+suite["ec"][20] = @benchmarkable expensive_computation(10)
+suite["ec"][30] = @benchmarkable expensive_computation(20)
 tune!(suite)
 results = run(suite,verbose = true)
 # Save the benchmark results to a JSON file
