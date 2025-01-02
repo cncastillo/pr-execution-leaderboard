da02858abc566f6726d6a4037c24356535fd036e
Stockless <mar.stockle@gmail.com>
2025-01-02 11:21:11 -0300

    test1

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -5,7 +5,7 @@ using MyPkg
 github_username = get(ENV, "GITHUB_ACTOR", "UNKNOWN")
 
 suite = BenchmarkGroup()
-suite["function1"][github_username] = @benchmarkable expensive_computation(30000)
+suite["function1"][github_username] = @benchmarkable expensive_computation(300000)
 tune!(suite)
 results = run(suite, verbose=true)
 
