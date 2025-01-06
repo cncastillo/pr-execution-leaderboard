5fcd5cc36a023037f7a8a02b8ec739d074386cae
github-action-benchmark <github@users.noreply.github.com>
2025-01-06 17:09:56 +0000

    add Julia benchmark result (julia) benchmark result for e22d7e5d71ae6c329bec9292eee9cf4ecfea0487

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736169274180,
+  "lastUpdate": 1736183396022,
   "repoUrl": "https://github.com/Stockless/pr-execution-leaderboard",
   "entries": {
     "Julia benchmark result": [
@@ -110,6 +110,38 @@ window.BENCHMARK_DATA = {
             "value": 1.563,
             "unit": "ns",
             "extra": "gctime=0.26523615086613406\nmemory=0\nallocs=0\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1000,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "committer": {
+            "name": "Stockless",
+            "username": "Stockless"
+          },
+          "id": "e22d7e5d71ae6c329bec9292eee9cf4ecfea0487",
+          "message": "test1",
+          "timestamp": "2025-01-06T17:08:12Z",
+          "url": "https://github.com/Stockless/pr-execution-leaderboard/pull/8/commits/e22d7e5d71ae6c329bec9292eee9cf4ecfea0487"
+        },
+        "date": 1736183395716,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "function1/Stockless",
+            "value": 1537042.5,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=1800112\nallocs=85002\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "function2/Stockless",
+            "value": 371173.5,
+            "unit": "ns",
+            "extra": "gctime=30703.166687844037\nmemory=123632\nallocs=52\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
