d6ca336362f5448ca7611ebfcb3a853fefdd7451
Stockless <mar.stockle@gmail.com>
2025-01-06 10:13:50 -0300

    test 2

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -8,13 +8,13 @@ github_username = get(ENV, "GITHUB_ACTOR", "UNKNOWN")
 
 # Define the benchmark
 suite = BenchmarkGroup()
-suite["function1"][github_username] = @benchmarkable expensive_computation(30000)
+#suite["function1"][github_username] = @benchmarkable expensive_computation(30000)
 suite["function2"][github_username] = @benchmarkable fast_function()
 
 # Tune and run the benchmark
 tune!(suite)
 results = run(suite, verbose=true)
 median_results = median(results)
-median_results["function1"][github_username].gctime = std(results["function1"][github_username]).time
+#median_results["function1"][github_username].gctime = std(results["function1"][github_username]).time
 median_results["function2"][github_username].gctime = std(results["function2"][github_username]).time
 BenchmarkTools.save("benchmarks/benchmark_results.json", median_results)
