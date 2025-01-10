9fbd3be45d84f62d085f32d19ea2f0bd33ebbcd9
David Cozmar <davidcozmar@gmail.com>
2025-01-10 17:46:36 -0300

    Add solution to the MyPkg package

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -1,5 +1,77 @@
 module MyPkg
+    using LinearAlgebra
+    struct Theoretical
+    end
+    struct ForwardEuler
+    end
+    GAMMA = 2 * π * 42.58 * 1e6 # rad / sT
+    M0 = 1.0
+    T1 = 1.0 # s
+    T2 = 0.5 # s
+    Bz = 1e-7 # T (tesla)
+    B = [0., 0., Bz]
+    xhat = [1., 0., 0.]
+    yhat = [0., 1., 0.]
+    zhat = [0., 0., 1.]
 
-# Your code here
+    """Return the magnetization vector for each timestep as a matrix.
 
-end  # module MyPkg
+    Progress the state of the magnetization m from its initial state m0
+    according to the type of the input argument `method`.
+
+    Included method types are Theoretical and ForwardEuler.
+    """
+    function solve(m0, dt, tmax, method)
+        Nsteps = floor(Int64, tmax / dt)
+        m = m0
+        mt = zeros(3, Nsteps)
+        # Note: missing m0 in the mt matrix!!
+        range = 1:Nsteps
+        for i in range
+            m = step(dt, m, method)
+            mt[:, i] = m
+        end
+        return mt
+    end
+
+    function theoretical_m(t)
+        mx = M0 * cos(GAMMA * Bz * t) * exp(-t / T2)
+        my = -M0 * sin(GAMMA * Bz * t) * exp(-t / T2)
+        mz = M0 * (1 - exp(-t / T1))
+        return [mx, my, mz]
+    end
+
+    """Return the magnetization vector for each timestep as a matrix.
+
+    Progress the state of the magnetization m from its initial state m0
+    according to the Theoretical solution using this Package's initial
+    conditions.
+    """
+    function solve(m0, dt, tmax, method::Theoretical)
+        Nsteps = floor(Int64, tmax / dt)
+        times = [dt * i for i in 1:Nsteps]'
+        broadcasted = theoretical_m.(times)
+        # The broadcasted result is a vector of vectors
+        # We have to horizontally concatenate these to make a matrix 3xNsteps matrix
+        mt = hcat(broadcasted...)
+        # Note: missing m0 in the mt matrix!! This is consistent with the other one
+        return mt
+    end
+
+    function bloch(m)
+        mx, my, mz = m
+
+        first = GAMMA * cross(m, B)
+        second = (mx * xhat + my * yhat) / T2
+        third = (mz - M0) / T1 * zhat
+
+        return first - second - third
+    end
+
+    function step(dt, m, method::ForwardEuler)
+        nextm = m + dt * bloch(m)
+        return nextm
+    end
+
+    export solve, step, Theoretical, ForwardEuler
+end
