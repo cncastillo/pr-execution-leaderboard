603756258acf08c05d3c89e36a3cbfa131e80708
Guillermo <guillermo.sahonero@gmail.com>
2025-01-10 17:46:05 -0300

    Testing changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,7 @@ m0   = [1.0, 0.0, 0.0]
 Δt   = 0.000001
 tmax = 3.0
 
-
 expected_result    = solve(m0, Δt, tmax, Theoretical())
 numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
-
 
 @test sum((expected_result-numerical_solution).^2)/length(expected_result)<0.5
