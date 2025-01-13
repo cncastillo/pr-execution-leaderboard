ad7b29e662aa22c0a816ce3ebcba9b823cc63cf8
fercoli <fiercoli@uc.cl>
2025-01-13 17:25:54 -0300

    just checking if those small changes do something

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -8,16 +8,11 @@ end
 struct ForwardEuler
 end
 
-g = 2 * π * 42.58 * 10^6
-Bz = 10^(-7)
-T1 = 1
-T2 = 0.5
-M0 = 1
 
 function solve(m0, dt, tmax, method)
     Nsteps = Int(tmax/dt)
     m = m0
-    mt = zeros(3,Nsteps)
+    mt = zeros(Float64,3,Nsteps)
     for i in 1:Nsteps
         m = step(dt, m, method)
         mt[:,i] = m
@@ -27,6 +22,11 @@ end
 
 function solve(m0, dt, tmax, method::Theoretical)
     t = (dt:dt:tmax)
+    g::Float64 = 2 * π * 42.58 * 10^6
+    Bz::Float64 = 10^(-7)
+    T1::Float64 = 1
+    T2::Float64 = 0.5
+    M0::Float64 = 1
     mt = hcat((x -> [m0[1]*cos(g*Bz*x)*exp(-x/T2); -m0[1]*sin(g*Bz*x)*exp(-x/T2); m0[1]*(1-exp(-x/T1))]).(t))
     result = zeros(Float64, length(mt), 3)
     for i in 1:length(mt)
@@ -36,6 +36,11 @@ function solve(m0, dt, tmax, method::Theoretical)
 end
 
 function bloch(M)
+    g::Float64 = 2 * π * 42.58 * 10^6
+    Bz::Float64 = 10^(-7)
+    T1::Float64 = 1
+    T2::Float64 = 0.5
+    M0::Float64 = 1
     return [-1/T2 g*Bz 0; -g*Bz -1/T2 0; 0 0 -1/T1] * M + [0 ; 0 ; M0/T1]
 end
 
