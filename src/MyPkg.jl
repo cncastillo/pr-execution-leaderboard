61c0b85caba7506545ef1a4948b8dbe7f6766775
Guillermo <guillermo.sahonero@gmail.com>
2025-01-13 17:09:55 -0300

    Testing Function 2

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -1,5 +1,5 @@
 module MyPkg
-using LinearAlgebra
+using StaticArrays
 
 struct Theoretical
 end
@@ -8,54 +8,52 @@ end
 struct RungeKutta2
 end
 
+const Bz = SVector{3, Float64}(0.0, 0.0, 1e-7)
+const γ = 2π*42.58e6::Float64
+const Bzz_γ = Bz[3]*γ::Float64
+const T1 = 1.00::Float64 
+const T1_i = 1/T1::Float64
+const T2 =  0.5::Float64
+const T2_i = 1/T2::Float64
+const M0 = 1.0::Float64
+const bloch_matrix = @SMatrix[  -T1_i      Bzz_γ    0;
+                                -Bzz_γ    -T2_i     0;
+                                0           0       -T1_i]
+const independent = @SVector[0, 0, M0*T1_i]
 
-function step(dt, m, method::ForwardEuler)
-    return m + bloch(m)*dt
+function step!(m::Vector{Float64}, Δt::Float64,  method::ForwardEuler)
+    m + bloch(m)*Δt
 end
 
 
-function step(dt, m, method::RungeKutta2)
-    m1 = m + bloch(m)*dt
-    return m + dt*(bloch(m1)+bloch(m))/2
+function step!(m::Vector{Float64}, Δt::Float64, method::RungeKutta2)
+    m1 = m + bloch(m)*Δt
+    m + Δt*(bloch(m1)+bloch(m))/2
 end
 
-function solve(m0, dt, tmax, method)
-    Nsteps = Int(round(tmax/dt))
+function solve(m0::Vector{Float64}, Δt, tmax, method)
+    Nsteps = Int(round(tmax/Δt))
     m = m0
-    mt = zeros(3, Nsteps)
-    for i in 1:Nsteps
-        m = step(dt, m, method)
-        mt[:, i] = m
+    mt = zeros(Nsteps, 3)
+    mt[1, :] = m0
+    for i in axes(mt, 1)
+        m = step!(m, Δt, method)
+        mt[i, :] = m
     end
     return mt
 end
 
-function solve(m0, dt, tmax, method::Theoretical)
-    Bz = [0.0, 0.0, 1e-7]       # T
-    γ = 2π*42.58e6              # rad/(sT)
-    T1 = 1                      # s
-    T2 = 0.5                    # s
-    t = dt:dt:tmax
-    m0 = m0[1]
-    mx = @. m0*cos(γ*Bz[1].*t)*exp.(-t./T2)
-    my = @. -m0*sin(γ*Bz[2]*t)*exp(-t/T2)
-    mz = @. m0*(1-exp(-t/T1))
-    return [mx my mz]'
+function solve(m0, Δt, tmax, method::Theoretical)
+    t = Δt:Δt:tmax
+    mx = @. m0[1].*cos.(γ*Bz[1].*t)*exp.(-t./T2)
+    my = @. -m0[1].*sin.(γ*Bz[2]*t)*exp.(-t./T2)
+    mz = @. m0[1]*(1-exp(-t/T1))
+    return [mx my mz]
 end
 
 function bloch(m)
-    Bz = [0.0, 0.0, 1e-7]       # T
-    γ = 2π*42.58e6              # rad/(sT)
-    T1 = 1                      # s
-    T2 = 0.5                    # s
-    M0 = [1.0, 0.0, 0.0]
-
-    MB = cross(γ*m,Bz)
-    Mx = MB[1] - m[1]/T2
-    My = MB[2] - m[2]/T2
-    Mz = MB[3] - (m[3] - M0[1])/T1
-
-    return [Mx, My, Mz]
+    M_new = bloch_matrix*m+independent
+    return M_new
 end
 
 export solve, step, Theoretical, ForwardEuler, RungeKutta2
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,4 @@ tmax = 3.0
 expected_result    = solve(m0, Δt, tmax, Theoretical())
 numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
 
-@test sum((expected_result-numerical_solution).^2)/length(expected_result)<0.5
+@test sum((expected_result-numerical_solution).^2)/length(expected_result)<1
