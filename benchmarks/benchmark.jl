3d6d907e88486ca2e230977e1f152813701466e0
David Cozmar <davidcozmar@gmail.com>
2025-01-13 18:20:19 -0300

    feat: activate benchmark of function2

--- benchmarks/benchmark.jl
+++ benchmarks/benchmark.jl
@@ -7,17 +7,17 @@ github_username = get(ENV, "GITHUB_ACTOR", "UNKNOWN")
 # Inputs
 m0   = [1.0, 0.0, 0.0]
 Δt   = 0.001
-tmax = 3.0
+t_max = 3.0
 
 # Define the benchmark
 suite = BenchmarkGroup()
-suite["function1"][github_username] = @benchmarkable solve(m0, Δt, tmax, ForwardEuler())
-# suite["function2"][github_username] = @benchmarkable solve(m0, Δt, t_max, params, ForwardEuler())
+# suite["function1"][github_username] = @benchmarkable solve(m0, Δt, tmax, ForwardEuler())
+suite["function2"][github_username] = @benchmarkable solve(m0, Δt, t_max, params, ForwardEuler())
 
 # Tune and run the benchmark
 tune!(suite)
 results = run(suite, verbose=true)
 median_results = median(results)
-median_results["function1"][github_username].gctime = std(results["function1"][github_username]).time
-# median_results["function2"][github_username].gctime = std(results["function2"][github_username]).time
+# median_results["function1"][github_username].gctime = std(results["function1"][github_username]).time
+median_results["function2"][github_username].gctime = std(results["function2"][github_username]).time
 BenchmarkTools.save("benchmarks/benchmark_results.json", median_results)
