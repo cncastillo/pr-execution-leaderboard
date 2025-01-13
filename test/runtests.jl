8e6e76762c71696d197bc996c80ba2b500e64666
fercoli <fiercoli@uc.cl>
2025-01-13 16:31:14 -0300

    first commit of MyPkg

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,11 @@ using MyPkg
 
 # Inputs
 m0   = [1.0, 0.0, 0.0]
-Δt   = 0.001
+Δt   = 0.000001
 tmax = 3.0
+
 
 expected_result    = solve(m0, Δt, tmax, Theoretical())
 numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
 
-@test numerical_solution ≈ expected_result
+@test numerical_solution ≈ expected_result atol=1e-1
