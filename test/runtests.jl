e966307d43f36af98dbea3ac0a65f49098e4fea1
Bastián Herrera <biherrera@uc.cl>
2025-01-13 17:55:18 -0300

    latest

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ m0   = [1.0, 0.0, 0.0]
 dt = 1e-3
 tmax = 3.0
 
-expected_result    = solve(m0, Δt, tmax, Theoretical())
-numerical_solution = solve(m0, Δt, tmax, ForwardEuler())
+expected_result    = solve(m0, dt, tmax, Theoretical())
+numerical_solution = solve(m0, dt, tmax, ForwardEuler())
 
 @test abs(solve(m0, dt, 3.0, ForwardEuler())[1, end] - solve(M0, dt, 3.0, Theoretical())[end, 1]) <= 1e-3
